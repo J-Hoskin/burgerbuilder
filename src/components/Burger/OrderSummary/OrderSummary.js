454c654e1531c0177cd21a74f906c769e06c2678
James Hoskin <J_hoskin@outlook.com>
2019-05-12 10:51:32 +0200

    Add order functionality

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Auxiliry from '../../../hoc/Auxiliry';
+import Button from '../../UI/Button/Button';
+
+const orderSummary = (props) => {
+    const ingredientSummary = Object.keys(props.ingredients)
+        .map(igKey => {
+            return (
+                <li key={igKey}>
+                    <span style={{textTransform: 'capitalize'}}>
+                        {igKey}
+                    </span>: {props.ingredients[igKey]}
+                </li>
+            )
+        });
+
+    return (
+        <Auxiliry>
+            <h3>Your Order</h3>
+            <p>A delicious burger with the following ingredients:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p><strong>{props.totalPrice.toFixed(2)}</strong></p>
+            <p>Continue to Checkout?</p>
+            <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
+            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+        </Auxiliry>
+    )
+};
+
+export default orderSummary;
